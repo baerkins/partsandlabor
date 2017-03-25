f484d740f208ef9baefa7503744c9e0c5ce157ef
Shaun Baer <shaun.baer@gmail.com>
2017-03-25 00:11:41 -0400

    Checkin

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,49 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var lint = require('gulp-sass-lint');
+
+var devurl  = 'pl.dev';
+var entry   = 'partsandlabor.scss';
+
+var gulp    = require('gulp');
+var sass    = require('gulp-sass');
+var lint    = require('gulp-sass-lint');
 var sassdoc = require('sassdoc');
+var bSync   = require('browser-sync').create();
 
-var entry = 'partsandlabor.scss';
 
-
-gulp.task('sass', function () {
-  return gulp.src(entry)
-    .pipe(lint())
-    .pipe(lint.format())
-    .pipe(lint.failOnError())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'));
+gulp.task( 'sass', function () {
+  return gulp.src( entry )
+    .pipe( lint() )
+    .pipe( lint.format() )
+    .pipe( lint.failOnError() )
+    .pipe( sass().on( 'error', sass.logError) )
+    .pipe( gulp.dest( './build/css' ) );
 });
 
-gulp.task('sassdoc', [''], function () {
+gulp.task('build_docs', function () {
   var options = {
     dest: 'docs',
     groups: {
-      'undefined': 'General',
       parts : 'Parts',
       labor : 'Labor'
     }
   }
   return gulp.src(['partsandlabor.scss', './labor/**/*.scss'])
-    .pipe(lint())
-    .pipe(lint.format())
-    .pipe(lint.failOnError())
-    .pipe(sassdoc(options));
+    .pipe( lint() )
+    .pipe( lint.format() )
+    .pipe( lint.failOnError() )
+    .pipe( sassdoc(options) );
+});
+
+gulp.task('demo_sass', function () {
+  return gulp.src('demo/scss/styles.scss')
+  .pipe( sass().on( 'error', sass.logError ) )
+  .pipe( gulp.dest('./demo/css' ) )
+  .pipe( bSync.stream() );
+});
+
+gulp.task('serve', function() {
+  bSync.init( {
+    server: "./demo"
+  } );
+  gulp.watch( 'demo/scss/*.scss', ['demo_sass'] );
+  gulp.watch( 'demo/*.html' ).on( 'change', bSync.reload );
 });
