7cdfb5ff77c20bef62ed27d73f1880bda2d362ee
Shaun Baer <shaun.baer@gmail.com>
2017-11-07 17:37:33 -0500

    Start React Docs

---
+++ docs_v3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItemNav from './item-nav';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ItemNav />
+    </div>
+  );
+};
+
+export default App;
